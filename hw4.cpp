6ab36e01fc511b5b85c42e60221fd9daae4f26a9
libos <benson60703@gmail.com>
2022-05-27 17:12:05 +0800

    fix: state check, error control

--- hw4.cpp
+++ hw4.cpp
@@ -12,7 +12,7 @@
 #include <map>
 #include <string>
 
-enum State {
+enum class State {
     NOT_LOADED,
     LOADED,
     RUNNING
@@ -20,47 +20,55 @@ enum State {
 
 struct user_regs_struct __regs;
 std::map<std::string, long int> regoffsets {
-    {"rax", ((unsigned char *) &__regs.rax) - ((unsigned char *) &__regs)},
-    {"rbx", ((unsigned char *) &__regs.rbx) - ((unsigned char *) &__regs)},
-    {"rcx", ((unsigned char *) &__regs.rcx) - ((unsigned char *) &__regs)},
-    {"rdx", ((unsigned char *) &__regs.rdx) - ((unsigned char *) &__regs)},
-    {"r8", ((unsigned char *) &__regs.r8) - ((unsigned char *) &__regs)},
-    {"r9", ((unsigned char *) &__regs.r9) - ((unsigned char *) &__regs)},
-    {"r10", ((unsigned char *) &__regs.r10) - ((unsigned char *) &__regs)},
-    {"r11", ((unsigned char *) &__regs.r11) - ((unsigned char *) &__regs)},
-    {"r12", ((unsigned char *) &__regs.r12) - ((unsigned char *) &__regs)},
-    {"r13", ((unsigned char *) &__regs.r13) - ((unsigned char *) &__regs)},
-    {"r14", ((unsigned char *) &__regs.r14) - ((unsigned char *) &__regs)},
-    {"r15", ((unsigned char *) &__regs.r15) - ((unsigned char *) &__regs)},
-    {"rdi", ((unsigned char *) &__regs.rdi) - ((unsigned char *) &__regs)},
-    {"rsi", ((unsigned char *) &__regs.rsi) - ((unsigned char *) &__regs)},
-    {"rbp", ((unsigned char *) &__regs.rbp) - ((unsigned char *) &__regs)},
-    {"rsp", ((unsigned char *) &__regs.rsp) - ((unsigned char *) &__regs)},
-    {"rip", ((unsigned char *) &__regs.rip) - ((unsigned char *) &__regs)},
+    {"rax",   ((unsigned char *) &__regs.rax)    - ((unsigned char *) &__regs)},
+    {"rbx",   ((unsigned char *) &__regs.rbx)    - ((unsigned char *) &__regs)},
+    {"rcx",   ((unsigned char *) &__regs.rcx)    - ((unsigned char *) &__regs)},
+    {"rdx",   ((unsigned char *) &__regs.rdx)    - ((unsigned char *) &__regs)},
+    {"r8",    ((unsigned char *) &__regs.r8)     - ((unsigned char *) &__regs)},
+    {"r9",    ((unsigned char *) &__regs.r9)     - ((unsigned char *) &__regs)},
+    {"r10",   ((unsigned char *) &__regs.r10)    - ((unsigned char *) &__regs)},
+    {"r11",   ((unsigned char *) &__regs.r11)    - ((unsigned char *) &__regs)},
+    {"r12",   ((unsigned char *) &__regs.r12)    - ((unsigned char *) &__regs)},
+    {"r13",   ((unsigned char *) &__regs.r13)    - ((unsigned char *) &__regs)},
+    {"r14",   ((unsigned char *) &__regs.r14)    - ((unsigned char *) &__regs)},
+    {"r15",   ((unsigned char *) &__regs.r15)    - ((unsigned char *) &__regs)},
+    {"rdi",   ((unsigned char *) &__regs.rdi)    - ((unsigned char *) &__regs)},
+    {"rsi",   ((unsigned char *) &__regs.rsi)    - ((unsigned char *) &__regs)},
+    {"rbp",   ((unsigned char *) &__regs.rbp)    - ((unsigned char *) &__regs)},
+    {"rsp",   ((unsigned char *) &__regs.rsp)    - ((unsigned char *) &__regs)},
+    {"rip",   ((unsigned char *) &__regs.rip)    - ((unsigned char *) &__regs)},
     {"flags", ((unsigned char *) &__regs.eflags) - ((unsigned char *) &__regs)},
 };
-
-
-void errquit(const char *msg) {
-	perror(msg);
-	exit(-1);
-}
 
 void exitProcess() {
     exit(0);
 }
 
 pid_t startTracee(char *progName, State &state) {
+    if (state != State::LOADED) {
+        printf("** state must be LOADED\n");
+        return -1;
+    }
     char *argv[] = {progName, NULL};
     pid_t child;
     int wait_State;
-    if ((child = fork()) < 0) errquit("fork");
+    if ((child = fork()) < 0) {
+        perror("** fork");
+        return -1;
+    }
     if (child == 0) {
-		if(ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) errquit("ptrace@child");
+		if(ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+            perror("** ptrace@child");
+            exit(1);
+        }
 		execv(argv[0], argv);
-		errquit("execv");
+        perror("** execv");
+		exit(1);
 	} else {
-        if(waitpid(child, &wait_State, 0) < 0) errquit("waitpid");
+        if(waitpid(child, &wait_State, 0) < 0) {
+            perror("** waitpid");
+            return -1;
+        }
 		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
         printf("** pid %d\n", child);
         state = State::RUNNING;
@@ -68,7 +76,28 @@ pid_t startTracee(char *progName, State &state) {
     return child;
 }
 
+bool contTracee(pid_t child, State &state) {
+    if (state != State::RUNNING) {
+        printf("** state must be RUNNING\n");
+        return false;
+    }
+    int wait_status;
+    ptrace(PTRACE_CONT, child, 0, 0);
+    waitpid(child, &wait_status, 0);
+    if (WIFEXITED(wait_status)) {
+        printf("** child process %d terminiated normally (code %d)\n",
+            child, WEXITSTATUS(wait_status));
+    } else if (WIFSTOPPED(wait_status)) {
+        // do something
+    }
+    return true;
+}
+
 bool loadProgram(int nargs, char **args, State &state, char *progName) {
+    if (state != State::NOT_LOADED) {
+        printf("** state must be NOT LOADED\n");
+        return false;
+    }
     if (nargs < 2) {
         printf("** no program path is given\n");
         return false;
@@ -86,7 +115,7 @@ bool loadProgram(int nargs, char **args, State &state, char *progName) {
         return false;
     }
     printf("** program '%s' loaded. entry point 0x%lx\n", args[1], header.e_entry);
-    strncpy(progName, args[1], sizeof(progName));
+    strncpy(progName, args[1], 256);
     state = State::LOADED;
     fclose(file);
     return true;
@@ -132,15 +161,19 @@ bool printSingleReg(pid_t child, State &state, char *regName) {
     return true;
 }
 
+char * getCmd(char *cmd) {
+    printf("sdb> ");
+    return fgets(cmd, 512, stdin);
+}
+
 
 int main() {
     State state = State::NOT_LOADED;
     char cmd[512];
     char progName[256];
     pid_t tracee = -1;
-    int wait_status;
-    printf("sdb> ");
-    while (fgets(cmd, sizeof(cmd), stdin) != NULL) {
+    
+    while (getCmd(cmd)!= NULL) {
         int nargs = 0;
         char *token, *saveptr, *args[3], *ptr = cmd;
         while (nargs < 3 && (token = strtok_r(ptr, " \t\n", &saveptr)) != NULL) {
@@ -151,21 +184,19 @@ int main() {
             // skip
         } else if (strcmp("load", args[0]) == 0) {
             loadProgram(nargs, args, state, progName);
-        } else if (strcmp("exit", args[0]) == 0) {
+        } else if ((strcmp("exit", args[0]) == 0) || (strcmp("q", args[0]) == 0)) {
             exitProcess();
         } else if (strcmp("start", args[0]) == 0) {
             tracee = startTracee(progName, state);
-        } else if (strcmp("cont", args[0]) == 0) {
-            ptrace(PTRACE_CONT, tracee, 0, 0);
-            waitpid(tracee, &wait_status, 0);
+        } else if ((strcmp("cont", args[0]) == 0) || (strcmp("c", args[0]) == 0)) {
+            contTracee(tracee, state);
         } else if (strcmp("getregs", args[0]) == 0) {
             printRegs(tracee, state);
-        } else if (strcmp("get", args[0]) == 0) {
+        } else if ((strcmp("get", args[0]) == 0) || (strcmp("g", args[0]) == 0)) {
             printSingleReg(tracee, state, args[1]);
         } else {
             printf("** undefined command\n");
         }
-        printf("sdb> ");
     }
 
     return 0;
