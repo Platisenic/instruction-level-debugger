5ecc57c7de9efa51b449c0493c7439e9fee1ad18
libos <benson60703@gmail.com>
2022-05-29 03:58:19 +0800

    feat: add breakpoints control & dump memory map

--- hw4.cpp
+++ hw4.cpp
@@ -12,6 +12,8 @@
 
 #include <map>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 enum class State {
     NOT_LOADED,
@@ -41,6 +43,20 @@ std::map<std::string, long int> regoffsets {
     {"flags", ((unsigned char *) &__regs.eflags) - ((unsigned char *) &__regs)},
 };
 
+struct BPinfo{
+    unsigned long address;
+    unsigned long oriByte;
+    BPinfo():
+        address(0), oriByte(0) {
+    }
+    BPinfo(unsigned long address, unsigned long oriByte):
+        address(address), oriByte(oriByte) {
+    }
+};
+
+std::vector<BPinfo> breakpoints;
+unsigned long lastBpAddress = 0;
+
 void exitProcess() {
     exit(0);
 }
@@ -53,12 +69,13 @@ pid_t startTracee(char *progName, State &state) {
     char *argv[] = {progName, NULL};
     pid_t child;
     int wait_State;
+    unsigned long code;
     if ((child = fork()) < 0) {
         perror("** fork");
         return -1;
     }
     if (child == 0) {
-		if(ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+		if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
             perror("** ptrace@child");
             exit(1);
         }
@@ -66,15 +83,75 @@ pid_t startTracee(char *progName, State &state) {
         perror("** execv");
 		exit(1);
 	} else {
-        if(waitpid(child, &wait_State, 0) < 0) {
+        if (waitpid(child, &wait_State, 0) < 0) {
             perror("** waitpid");
             return -1;
         }
 		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
-        printf("** pid %d\n", child);
+        for (auto it = breakpoints.begin(); it != breakpoints.end(); it++) {
+            code = ptrace(PTRACE_PEEKTEXT, child, it->address, 0);
+            code = (code & 0xffffffffffffff00) | 0xcc;
+            if (ptrace(PTRACE_POKETEXT, child, it->address, code) != 0) {
+                perror("** poketest");
+                return -1;
+            }
+        }
         state = State::RUNNING;
+        printf("** pid %d\n", child);
     }
     return child;
+}
+
+bool stepTracee(pid_t child, State &state) {
+    if (state != State::RUNNING) {
+        printf("** state must be RUNNING\n");
+        return false;
+    }
+    int wait_status;
+    unsigned long rip, code;
+    if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) {
+        perror("** singlestep");
+        return false;
+    }
+    waitpid(child, &wait_status, 0);
+    if (WIFEXITED(wait_status)) {
+        printf("** child process %d terminiated normally (code %d)\n",
+            child, WEXITSTATUS(wait_status));
+        state = State::LOADED;
+    } else if (WIFSTOPPED(wait_status)) {
+        unsigned long address = lastBpAddress;
+        auto it = find_if(breakpoints.begin(), breakpoints.end(), [address](const BPinfo &b) {
+            return b.address == address;
+        });
+        if (it != breakpoints.end()) {
+            code = ptrace(PTRACE_PEEKTEXT, child, it->address, 0);
+            code = (code & 0xffffffffffffff00) | 0xcc;
+            if (ptrace(PTRACE_POKETEXT, child, it->address, code) != 0) {
+                perror("** poketest");
+                return false;
+            }
+            lastBpAddress = 0;
+        }
+        rip = ptrace(PTRACE_PEEKUSER, child, regoffsets["rip"], 0);
+        it = find_if(breakpoints.begin(), breakpoints.end(), [rip](const BPinfo &b) {
+            return b.address == (rip - 1);
+        });
+        if (it != breakpoints.end()) {
+            code = ptrace(PTRACE_PEEKTEXT, child, it->address, 0);
+            code = (code & 0xffffffffffffff00) | it->oriByte;
+            if (ptrace(PTRACE_POKETEXT, child, it->address, code) != 0) {
+                perror("** poketest");
+                return false;
+            }
+            if (ptrace(PTRACE_POKEUSER, child, regoffsets["rip"], rip - 1) < 0) {
+                perror("** pokeuser");
+                return false;
+            }
+            lastBpAddress = it->address;
+            printf("** breakpoint @\n"); // todo dissasm
+        }
+    }
+    return true;
 }
 
 bool contTracee(pid_t child, State &state) {
@@ -82,7 +159,14 @@ bool contTracee(pid_t child, State &state) {
         printf("** state must be RUNNING\n");
         return false;
     }
+    if (lastBpAddress != 0) {
+        stepTracee(child ,state);
+        if (lastBpAddress != 0) { // encounter breakpoints in stepTracee
+            return true;
+        }
+    }
     int wait_status;
+    unsigned long rip, code;
     if(ptrace(PTRACE_CONT, child, 0, 0) < 0) {
         perror("** cont");
         return false;
@@ -93,7 +177,24 @@ bool contTracee(pid_t child, State &state) {
             child, WEXITSTATUS(wait_status));
         state = State::LOADED;
     } else if (WIFSTOPPED(wait_status)) {
-        // do something
+        rip = ptrace(PTRACE_PEEKUSER, child, regoffsets["rip"], 0);
+        auto it = find_if(breakpoints.begin(), breakpoints.end(), [rip](const BPinfo &b) {
+            return b.address == (rip - 1);
+        });
+        if (it != breakpoints.end()) {
+            code = ptrace(PTRACE_PEEKTEXT, child, it->address, 0);
+            code = (code & 0xffffffffffffff00) | it->oriByte;
+            if (ptrace(PTRACE_POKETEXT, child, it->address, code) != 0) {
+                perror("** poketest");
+                return false;
+            }
+            if (ptrace(PTRACE_POKEUSER, child, regoffsets["rip"], rip - 1) < 0) {
+                perror("** pokeuser");
+                return false;
+            }
+            lastBpAddress = it->address;
+            printf("** breakpoint @\n"); // todo dissasm
+        }
     }
     return true;
 }
@@ -105,30 +206,10 @@ bool runTracee(char *progName, pid_t child, State &state) {
     }
     if (state == State::LOADED) {
         child = startTracee(progName, state);
-    }
-    if (state == State::RUNNING) {
         contTracee(child, state);
-    }
-    return true;
-}
-
-bool stepTracee(pid_t child, State &state) {
-    if (state != State::RUNNING) {
-        printf("** state must be RUNNING\n");
-        return false;
-    }
-    int wait_status;
-    if(ptrace(PTRACE_SINGLESTEP, child, 0, 0) < 0) {
-        perror("** singlestep");
-        return false;
-    }
-    waitpid(child, &wait_status, 0);
-    if (WIFEXITED(wait_status)) {
-        printf("** child process %d terminiated normally (code %d)\n",
-            child, WEXITSTATUS(wait_status));
-        state = State::LOADED;
-    } else if (WIFSTOPPED(wait_status)) {
-        // do something
+    } else if (state == State::RUNNING) {
+        printf("** program %s is already running\n", progName);
+        contTracee(child, state);
     }
     return true;
 }
@@ -199,13 +280,13 @@ bool getSingleReg(int nargs, pid_t child, State &state, char *regName) {
         printf("** undefined register\n");
         return false;
     }
-    unsigned long long regVal;
+    unsigned long regVal;
     regVal = ptrace(PTRACE_PEEKUSER, child, iter->second, 0);
-    printf("%s = %lld (0x%llx)\n", regName, regVal, regVal);
+    printf("%s = %ld (0x%lx)\n", regName, regVal, regVal);
     return true;
 }
 
-bool setSingleTreg(int nargs, pid_t child, State &state, char *regName, char *regValptr) {
+bool setSingleReg(int nargs, pid_t child, State &state, char *regName, char *regValptr) {
     if (nargs < 3) {
         printf("** Not enough input arguments\n");
         return false;
@@ -220,7 +301,7 @@ bool setSingleTreg(int nargs, pid_t child, State &state, char *regName, char *re
         printf("** undefined register\n");
         return false;
     }
-    unsigned long long regVal = strtoull(regValptr, NULL, 16);
+    unsigned long regVal = strtoul(regValptr, NULL, 16);
     if (ptrace(PTRACE_POKEUSER, child, iter->second, regVal) < 0) {
         perror("** pokeuser");
         return false;
@@ -237,14 +318,14 @@ bool dumpMemory(int nargs, pid_t child, State &state, char *address) {
         printf("** state must be RUNNING\n");
         return false;
     }
-    unsigned long long addressVal = strtoull(address, NULL, 16);
+    unsigned long addressVal = strtoul(address, NULL, 16);
     long ret;
     unsigned char *ptr = (unsigned char *) &ret;
     unsigned char strbuf[17];
     memset(strbuf, 0, 17);
 
     for (int i=0;i<10;i++) {
-        if (i%2==0) { printf("      0x%llx:", addressVal + i*8); }
+        if (i%2==0) { printf("      0x%lx:", addressVal + i*8); }
         ret = ptrace(PTRACE_PEEKTEXT, child, addressVal + i*8, 0);
         for (int j=0;j<8;j++) {
             printf(" %2.2x", ptr[j]);
@@ -254,6 +335,104 @@ bool dumpMemory(int nargs, pid_t child, State &state, char *address) {
             printf(" |%s|\n", strbuf);
         }
     }
+    return true;
+}
+
+bool dumpVMMap(pid_t child, State &state) {
+    if (state != State::RUNNING) {
+        printf("** state must be RUNNING\n");
+        return false;
+    }
+    char buf[512];
+    char mmap_path[128];
+    FILE *fp;
+    long vm_begin, vm_end, offset;
+
+    snprintf(mmap_path, sizeof(mmap_path), "/proc/%u/maps", child);
+    if ((fp = fopen(mmap_path, "rt")) == NULL) { perror("** fopen"); return false; }
+    while (fgets(buf, sizeof(buf), fp) != NULL) {
+        int nargs = 0;
+        char *token, *saveptr, *args[8], *ptr = buf;
+        while (nargs < 8 && (token = strtok_r(ptr, " \t\n\r", &saveptr)) != NULL) {
+			args[nargs++] = token;
+			ptr = NULL;
+		}
+        if (nargs < 6) continue;
+        if ((ptr = strchr(args[0], '-')) == NULL) { continue; }
+        *ptr = '\0';
+        vm_begin = strtol(args[0], NULL, 16);
+        vm_end = strtol(ptr+1, NULL, 16);
+    
+        offset = strtol(args[2], NULL, 16);
+        if (strlen(args[1]) < 4) { continue; }
+        args[1][3] = '\0';
+        printf("%016lx-%016lx %s %-8lx %s\n", vm_begin, vm_end, args[1], offset, args[5]);
+    }
+    return true;
+}
+
+bool setBrackPoint(int nargs, pid_t child, State &state, char *address) {
+    if (nargs < 2) {
+        printf("** no addr is given\n");
+        return false;
+    }
+    if (state != State::RUNNING) {
+        printf("** state must be RUNNING\n");
+        return false;
+    }
+    // check text range
+
+    unsigned long addressVal = strtoul(address, NULL, 16);
+    auto it = find_if(breakpoints.begin(), breakpoints.end(), [addressVal](const BPinfo &b) {
+        return b.address == addressVal;
+    });
+    if (it != breakpoints.end()) {
+        printf("** the breakpoint is already exists. (breakpoint %ld)\n", std::distance(breakpoints.begin(), it));
+        return false;
+    }
+    unsigned long code = ptrace(PTRACE_PEEKTEXT, child, addressVal, 0);
+    unsigned long oriByte = code & 0xff;
+    unsigned long patchedCode = (code & 0xffffffffffffff00) | 0xcc;
+    if (ptrace(PTRACE_POKETEXT, child, addressVal, patchedCode) != 0) {
+        perror("** poketest");
+        return false;
+    }
+    breakpoints.push_back(BPinfo(addressVal, oriByte));
+    
+    return true;
+}
+
+bool listBrackPoints() {
+    for (auto it = breakpoints.begin(); it != breakpoints.end(); it++) {
+        printf("%3ld: %8lx\n", std::distance(breakpoints.begin(), it), it->address);
+    }
+    return true;
+}
+
+bool deleteBrackPoint(int nargs, pid_t child, State &state, char *bpID) {
+    if (nargs < 2) {
+        printf("** no addr is given\n");
+        return false;
+    }
+    if (state != State::RUNNING) {
+        printf("** state must be RUNNING\n");
+        return false;
+    }
+    unsigned long bpnum = strtoul(bpID, NULL, 10);
+    if (bpnum >= breakpoints.size()) {
+        printf("** breakpoint %ld does not exist\n", bpnum);
+        return false;
+    }
+    auto it = breakpoints.begin();
+    std::advance(it, bpnum);
+    unsigned long code = ptrace(PTRACE_PEEKTEXT, child, it->address, 0);
+    code = (code & 0xffffffffffffff00) | it->oriByte;
+    if (ptrace(PTRACE_POKETEXT, child, it->address, code) != 0) {
+        perror("** poketest");
+        return false;
+    }
+    breakpoints.erase(it);
+    printf("** breakpoint %ld deleted\n", bpnum);
     return true;
 }
 
@@ -316,11 +495,19 @@ int main() {
         } else if ((strcmp("get", args[0]) == 0) || (strcmp("g", args[0]) == 0)) {
             getSingleReg(nargs, tracee, state, args[1]);
         } else if ((strcmp("set", args[0]) == 0) || (strcmp("s", args[0]) == 0)) {
-            setSingleTreg(nargs, tracee, state, args[1], args[2]);
+            setSingleReg(nargs, tracee, state, args[1], args[2]);
         } else if ((strcmp("help", args[0]) == 0) || (strcmp("h", args[0]) == 0)) {
             printHelpMsg();
         } else if ((strcmp("dump", args[0]) == 0) || (strcmp("x", args[0]) == 0)) {
             dumpMemory(nargs, tracee, state, args[1]);
+        } else if ((strcmp("vmmap", args[0]) == 0) || (strcmp("m", args[0]) == 0)) {
+            dumpVMMap(tracee, state);
+        } else if ((strcmp("break", args[0]) == 0) || (strcmp("b", args[0]) == 0)) {
+            setBrackPoint(nargs, tracee, state, args[1]);
+        } else if ((strcmp("list", args[0]) == 0) || (strcmp("l", args[0]) == 0)) {
+            listBrackPoints();
+        } else if (strcmp("delete", args[0]) == 0) {
+            deleteBrackPoint(nargs, tracee, state, args[1]);
         } else {
             printf("** undefined command\n");
         }
